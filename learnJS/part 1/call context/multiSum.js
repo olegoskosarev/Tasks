df5b0c7df1f59f2310c1d4feb6ba3279c557b402
olegoskosarev <olegoskosarev@gmail.com>
2019-01-31 15:20:31 +0300

    Added solution of 'multi sum'

---
+++ learnJS/part 1/call context/multiSum.js
@@ -0,0 +1,17 @@
+function sum(a) {
+
+    let result = a;
+
+    function sub(b) {
+        result += b;
+        return sub;
+    }
+
+    sub.toString =  function() {
+        return result;
+    }
+
+    return sub;
+}
+
+console.log(sum(0)(1)(2)(3)(4)(5)); // 15.
